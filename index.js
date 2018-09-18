c711c90958a156c8ea118c1b3dde4e0c4cadd61a
sneakers-the-rat <JLSaunders987@gmail.com>
2018-09-18 00:40:11 -0700

    seein if they go together

---
+++ index.js
@@ -0,0 +1,12 @@
+$(window).on("load", function(){
+    $('.cd-panel').removeClass('is-visible');
+    $('.cd-panel').addClass('is-visible');
+
+});
+/*
+$(window).bind('beforeunload', function(){
+    $('.cd-panel').removeClass('is-visible');
+});
+*/
+
+
